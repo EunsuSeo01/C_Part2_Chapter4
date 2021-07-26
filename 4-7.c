cf771cec9cf62f95bcd1fa0f3177f0c4f816e4ed
EunsuSeo01 <eunsu0613@naver.com>
2021-07-27 00:32:59 +0900

    0727 first commit

--- 4-7.c
+++ 4-7.c
@@ -1,8 +1,21 @@
 /*
- last modified 2021-07-26
+ 포인터 변수를 배열처럼 사용할 수 있지만 둘은 같은 것은 아님.
+ -> 모든 포인터 변수의 크기 : (32bit 컴파일러에서) 4 byte로 고정되어 있음.
+    배열의 크기 : 배열 길이에 따라 달라짐.
+ last modified 2021-07-27
 */
 #include <stdio.h>
 int main(void) {
+	int array[3] = { 10, 20, 30 };
+	int* p = NULL;
+	p = array;
+
+	printf("%d %d %d \n", array[0], array[1], array[2]);
+	printf("%d %d %d \n", *(array + 0), *(array + 1), *(array + 2));
+	printf("%d %d %d \n", p[0], p[1], p[2]);
+	printf("%d %d %d \n", *(p + 0), *(p + 1), *(p + 2));
+
+	printf("배열의 크기 : %d 포인터의 크기 : %d \n", sizeof(array), sizeof(p));
 
 	return 0;
 }
---
+++ 4-8.c
@@ -0,0 +1,23 @@
+/*
+ *p, p[0]의 값이 p에 저장된 주소값에 따라 달라지는 것을 보여주는 예제.
+ last modified 2021-07-27
+*/
+#include <stdio.h>
+int main(void) {
+	int array[3] = { 10, 20, 30 };
+	int* p = NULL;
+
+	p = array;
+	printf("%d %d %d \n", p[0], p[1], p[2]);
+	printf("%d %d %d \n", *p, *(p + 1), *(p + 2));
+
+	p = array + 1;
+	printf("%d %d %d \n", p[-1], p[0], p[1]);
+	printf("%d %d %d \n", *(p - 1), *p, *(p + 1));
+
+	p = array + 2;
+	printf("%d %d %d \n", p[-2], p[-1], p[0]);
+	printf("%d %d %d \n", *(p - 2), *(p - 1), *p);
+
+	return 0;
+}
