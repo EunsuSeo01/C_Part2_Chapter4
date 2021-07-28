43edfcf65c6754d25b581d5cbaea53bf13020414
EunsuSeo01 <eunsu0613@naver.com>
2021-07-28 19:22:46 +0900

    0728 first commit

--- 4-10.c
+++ 4-10.c
@@ -1,8 +1,29 @@
 /*
- last modified 2021-07-27
+ 포인터 변수 p를 고정한 상태에서 배열의 메모리 공간에 접근하는 방식을 보여주는 예제.
+ last modified 2021-07-28
 */
 #include <stdio.h>
 int main(void) {
+	int array[3];	// 초기화 안 함.
+	int* p = NULL;
+
+	p = array;	// 포인터 p를 이용하여 배열 array에 접근 가능해짐.
+
+	*p = 10;
+	printf("%d %d %d \n", p[0], p[1], p[2]);
+	printf("--------------\n");
+
+	*(p + 1) = 20;	// p는 고정해 둔 상태에서 +를 통해 주소 이동 후 값을 참조.
+	printf("%d %d %d \n", p[0], p[1], p[2]);
+	printf("--------------\n");
+
+	*(p + 2) = 30;
+	printf("%d %d %d \n", p[0], p[1], p[2]);
+	printf("--------------\n");
+
+	printf("%d %d %d \n", *p, *(p + 1), *(p + 2));
+	printf("%d %d %d \n", p[0], p[1], p[2]);
+	printf("--------------\n");
 
 	return 0;
 }
